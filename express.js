afb1213fb879efb7b83e9cd4d84f69399839cc6b
The-Palette <mrq.palette@gmail.com>
2023-09-13 09:48:24 +0000

    first commit

---
+++ express.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+
+//Get
+app.get('/', function (req, res) {
+  res.send('Hello World22222!!!!')
+})
+
+//GET=> param
+app.get('/dog/:color', function (req, res) {
+  const data = req.params
+  //console.log("color : ", data)
+  res.send('<h1 style="color:'+ data.color +';"> 멍멍!! </h1>')
+})
+// https://~/dog/red
+
+//GET=> query
+app.get('/cat', function (req, res) {
+  const data = req.query
+  console.log("color : ", data)
+  res.send('<h1 style="color:'+ data.color +';"> 야~옹 </h1>')
+})
+// https://~/cat?color=blue
+
+
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(3000)
---
+++ figelt.js
@@ -0,0 +1,29 @@
+var figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+figlet.text(
+  "MrQ!",
+  {
+    font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
